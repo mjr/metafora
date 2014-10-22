6064bfed4ea7facd1bfcdb5a03f80baca4d97512
felipevianna86 <felipe.vianna86@gmail.com>
2014-10-22 13:30:28 -0300

    Listar MAtricula

--- grails-app/controllers/br/gov/rn/saogoncalo/academico/MatriculaController.groovy
+++ grails-app/controllers/br/gov/rn/saogoncalo/academico/MatriculaController.groovy
@@ -3,4 +3,9 @@ package br.gov.rn.saogoncalo.academico
 class MatriculaController {
 
     def index() { }
+	
+	def listar() {
+		def matriculas = Matricula.findAll()
+		render (view:"/matricula/listarMatricula.gsp", model:[matriculas:matriculas])
+	}
 }
